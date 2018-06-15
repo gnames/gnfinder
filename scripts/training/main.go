df77867930af2b48ebfe10bc4221b9e353dddf9b
Dmitry Mozzherin <dmozzherin@gmail.com>
2018-06-15 13:04:11 -0500

    add training back

---
+++ scripts/training/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/gnames/gnfinder/dict"
+	"github.com/gnames/gnfinder/nlp"
+	"github.com/gnames/gnfinder/util"
+)
+
+func main() {
+	dir := filepath.Join("..", "..", "data")
+	data := nlp.NewTrainingLanguageData(filepath.Join(dir, "training"))
+	output := filepath.Join(dir, "files", "nlp")
+	d := dict.LoadDictionary()
+	for lang, v := range data {
+		path := filepath.Join(output, lang.String(), "bayes.json")
+		nb := nlp.Train(v, &d)
+		err := ioutil.WriteFile(path, nb.Dump(), 0644)
+		util.Check(err)
+	}
+}
