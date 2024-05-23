9fea3a3eebfd5f4dba60129f45182c87086a9959
Dmitry Mozzherin <dmozzherin@gmail.com>
2024-05-23 19:03:51 +0000

    update dicts, add Ruhoff data-source

--- pkg/io/dict/dict_test.go
+++ pkg/io/dict/dict_test.go
@@ -11,21 +11,21 @@ var dictionary = dict.LoadDictionary()
 
 func TestInAmbigUninomials(t *testing.T) {
 	l := len(dictionary.InAmbigUninomials)
-	assert.Equal(t, 214, l)
+	assert.Equal(t, 222, l)
 	_, ok := dictionary.InAmbigUninomials["Minimi"]
 	assert.True(t, ok)
 }
 
 func TestCommonWords(t *testing.T) {
 	l := len(dictionary.CommonWords)
-	assert.Equal(t, 70791, l)
+	assert.Equal(t, 70792, l)
 	_, ok := dictionary.CommonWords["all"]
 	assert.True(t, ok)
 }
 
 func TestInGenera(t *testing.T) {
 	l := len(dictionary.InGenera)
-	assert.Equal(t, 525376, l)
+	assert.Equal(t, 528731, l)
 	_, ok := dictionary.InGenera["Plantago"]
 	assert.True(t, ok)
 }
